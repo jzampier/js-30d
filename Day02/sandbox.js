5c933382023445190cd98f40563db633e4eab324
Julio Zampier <juliozampier@gmail.com>
2023-01-29 15:21:30 -0300

    D02 data types

---
+++ Day02/sandbox.js
@@ -0,0 +1,158 @@
+// let numberOne = 3;
+// let numberTwo = 3;
+
+// console.log(numberOne === numberTwo);
+
+// let js = 'JavaScript';
+// let py = 'Python';
+
+// console.log(js == py);
+
+// let lightOn = true;
+// let lightOff = false;
+
+// console.log(lightOn == lightOff);
+
+// let nums = [1, 2, 3];
+// nums[0] = 10;
+// console.log(nums);
+
+// let numbers = [1, 2, 3];
+// console.log(nums == numbers);
+
+// let userOne = {
+//   name: 'Julio',
+//   role: 'Unemployed',
+//   country: 'Brazil',
+// };
+// let userTwo = {
+//   name: 'Julio',
+//   role: 'Unemployed',
+//   country: 'Brazil',
+// };
+
+// console.log(userOne == userTwo);
+
+// let numbers = nums;
+// console.log(nums == numbers);
+
+// let userTwo = userOne;
+// console.log(userOne == userTwo);
+
+// !Declaring Number Data Types
+// let age = 35;
+// const gravity = 9.81;
+// let mass = 72;
+// const PI = 3.14;
+
+// const boilingPoint = 100;
+// const bodyTemp = 37;
+
+// console.log(age, gravity, mass, PI, boilingPoint, bodyTemp);
+
+// !Math Object
+// const PI = Math.PI;
+// let numbers = [-5, 3, 20, 4, 5, 10];
+// console.log(PI);
+
+// //round above if greater than .5 and round below if less than .5
+// console.log(Math.round(PI)); // auto
+// console.log(Math.round(9.81)); // auto
+// console.log(Math.floor(PI)); // rounds down
+// console.log(Math.ceil(PI)); // rounds above
+
+// console.log(Math.min(-5, 3, 20, 4, 5, 10)); //Min
+// console.log(Math.max(-5, 3, 20, 4, 5, 10)); //Max
+
+// const randNum = Math.random(); // random number between 0 and 0.999999
+// console.log(randNum);
+
+// const num = Math.floor(Math.random() * 100); // random number between 0 and 100
+// console.log(num);
+
+// // Absolute value
+// console.log(Math.abs(-10));
+// // Square root
+// console.log(Math.sqrt(100));
+// console.log(Math.sqrt(2));
+// // Power
+// console.log(Math.pow(3, 2));
+// console.log(Math.E);
+// // Logarithm
+// // Natural logarithm with base E of x
+// console.log(Math.log(2));
+// console.log(Math.log(10));
+// // Natural logarithm of 2 and 10
+// console.log(Math.LN2);
+// console.log(Math.LN10);
+// // Trigonometry
+// console.log(Math.sin(0))
+// console.log(Math.sin(60))
+// console.log(Math.cos(0))
+// console.log(Math.cos(60))
+
+// !Strings
+// let space = ' ';
+// let firstName = 'Julio';
+// let lastName = 'Zampier';
+// let age = 250;
+// let country = 'Brazil';
+// let city = 'Macae';
+// let language = 'Javascript';
+// let job = 'unemployed';
+// let quote = 'Camarao que dorme "a onda leva" kkk "yadson"';
+// let quoteWithBackTick = `Arroy 'tchacarhay'.`;
+
+// // Concatenation
+// let fullName = firstName + space + lastName;
+// console.log(fullName, '\n');
+// console.log(fullName + '. I am ' + age + '. I live in ' + country, '\n');
+// console.log(`${fullName}. I'm ${age}. I live in ${country}\n`);
+
+// const paragraph =
+//   'My name is Asabeneh Yetayeh. I live in Finland, Helsinki.\
+// I am a teacher and I love teaching. I teach HTML, CSS, JavaScript, React, Redux, \
+// Node.js, Python, Data Analysis and D3.js for anyone who is interested to learn. \
+// In the end of 2019, I was thinking to expand my teaching and to reach \
+// to global audience and I started a Python challenge from November 20 - December 19.\
+// It was one of the most rewarding and inspiring experience.\
+// Now, we are in 2020. I am enjoying preparing the 30DaysOfJavaScript challenge and \
+// I hope you are enjoying too.';
+
+// console.log(paragraph);
+// console.log(
+//   'I hope everyone is enjoying the 30 Days Of JavaScript challenge.\nDo you?'
+// );
+// console.log('Days\tTopics\tExercidses');
+// console.log('Day 1\t3\t5');
+// console.log('Day 2\t3\t5');
+// console.log('Day 3\t3\t5');
+// console.log('Day 4\t3\t5');
+// console.log('This is a backslash symbol (\\)');
+// console.log('In every programming language it starts with "Hello, World!"');
+// console.log("In every programming language it starts with 'Hello, World!'");
+// console.log("The saying 'Seeing is Believing' isn't correct in 2020");
+
+//!Template Literals (template Strings)
+// console.log(`The sum of 2 and 3 is 5`);
+// let a = 2;
+// let b = 3;
+// console.log(`The sum of ${a} and ${b} is ${a + b}`);
+
+// let firstName = 'Julio';
+// let lastName = 'Zampier';
+// let country = 'Brazil';
+// let city = 'Macae';
+// let language = 'JavaScript';
+// let job = 'Unemployed';
+// let age = 41;
+// let fullName = firstName + ' ' + lastName;
+// let personInfoTwo = `I am ${fullName}. I am ${age}. I live in ${country}.`;
+// let personInfoThree = `I am ${fullName}. I live in ${city}, ${country} I'm an \
+// ${job}. I study ${language}.`;
+// console.log(personInfoTwo, '\n');
+// console.log(personInfoThree, '\n');
+
+// console.log(`${a} is greater than ${b}: ${a>b}`);
+
+//!String Methods

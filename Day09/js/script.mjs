1338c92f32cb3ad97f7ba150f659a29fbf713b2c
Julio Zampier <juliozampier@gmail.com>
2023-05-27 20:16:17 -0300

    lv3 - 4

--- Day09/js/script.mjs
+++ Day09/js/script.mjs
@@ -551,3 +551,19 @@ console.log(findTopLanguages(countryData, 10)); */
 
 /* 3 - Use countries_data.js file create a function which create the ten most
 populated countries */
+/* function sortCountriesByPop(arr, qty) {
+  const sortedByPopulation = arr.sort((a, b) => b.population - a.population);
+  const contriesPop = sortedByPopulation.map((element) => {
+    return { name: element.name, population: element.population };
+  });
+
+  return contriesPop.slice(0, qty);
+}
+console.log(sortCountriesByPop(countryData, 3)); */
+
+/* 4 - Try to develop a program which calculate measure of central tendency of
+a sample(mean, median, mode) and measure of variability(range, variance,
+standard deviation). In addition to those measures find the min, max, count,
+percentile, and frequency distribution of the sample. You can create an object
+called statistics and create all the functions which do statistical
+calculations as method for the statistics object. Check the output below. */

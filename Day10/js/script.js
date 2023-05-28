f3b4d420129379712206989db7c21454d6d5cec2
Julio Zampier <juliozampier@gmail.com>
2023-05-28 15:22:26 -0300

    lets start with excercises

--- Day10/js/script.js
+++ Day10/js/script.js
@@ -70,8 +70,8 @@ for (const language of setOfLanguages){
 // console.log(setOfNumbers.size);
 
 //? Union of Sets A U B
-let a = [1, 2, 3, 4, 5];
-let b = [3, 4, 5, 6];
+// let a = [1, 2, 3, 4, 5];
+// let b = [3, 4, 5, 6];
 // let c = [...a, ...b];
 // console.log(c);
 // let A = new Set(a);
@@ -80,10 +80,53 @@ let b = [3, 4, 5, 6];
 // console.log(C);
 
 //? Intersection of Sets A n B
-let A = new Set(a);
-let B = new Set(b);
-let c = a.filter((num) => B.has(num));
-let C = new Set(c);
-console.log(C);
+// let A = new Set(a);
+// let B = new Set(b);
+// let c = a.filter((num) => B.has(num));
+// let C = new Set(c);
+// console.log(C);
 
 //? Difference of sets A\B
+// let A = new Set(a);
+// let B = new Set(b);
+
+// let c = a.filter((num)=> !B.has(num))
+// let C = new Set(c);
+// console.log(`LOG ~ C:`, C)
+
+//! Maps -> Collection of key value pairs
+//? Creating a map
+// const map = new Map();
+// console.log(`LOG ~ map:`, map)
+//? Creating a map from array
+const countries = [
+  ['Finland', 'Helsinki'],
+  ['Sweden', 'Stockholm'],
+  ['Norway', 'Oslo'],
+];
+const map = new Map(countries);
+console.log(`LOG ~ map:`, map);
+console.log(`LOG ~ map size:`, map.size);
+
+//? Adding values to the Map .set
+const countriesMap = new Map();
+countriesMap.set('Finland', 'Helsinki');
+countriesMap.set('Sweden', 'Stockholm');
+countriesMap.set('Norway', 'Oslo');
+console.log(countriesMap.size);
+console.log(countriesMap);
+
+//?  Getting values from the Map .get
+console.log(`Capital of Norway =>`, countriesMap.get('Norway'));
+
+//? Checking if a key exists in the Map .has
+console.log(countriesMap.has('Finland'));
+
+//? Getting all values from map using loop
+countriesMap.forEach((capital, country) =>
+  console.log(`Pais: ${country}, Capital: ${capital}`)
+);
+
+//? Deleting values from the Map .delete`))
+console.log(countriesMap.delete('Norway'));
+console.log(countriesMap);

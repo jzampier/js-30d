a9ff0ecd3c2dc4a242828ca79c54d17b97204604
Julio Zampier <juliozampier@gmail.com>
2023-05-30 22:42:48 -0300

    Day 11 done

--- Day11/js/script.js
+++ Day11/js/script.js
@@ -234,24 +234,25 @@
 // console.log(sumAllNumbers(1, 2, 3, 4, 5, 6));
 
 //!Exercises
-import { constants, countries, rectangle, users } from './consts.js';
+//import { constants, countries, rectangle, users } from './consts.js';
+import { countries } from './countries_data.js';
 //! Lv-1
 // 1 Destructure and assign the elements of constants array to e, pi, gravity,
 //humanBodyTemp, waterBoilingTemp.
-let [e, pi, gravity, hbt, wbt] = constants;
-console.log(e, pi, gravity, hbt, wbt);
+// let [e, pi, gravity, hbt, wbt] = constants;
+// console.log(e, pi, gravity, hbt, wbt);
 
 // 2 Destructure and assign the elements of countries array to fin, est, sw,
 //den, nor
-let [fin, est, sw, den, nor] = countries;
-console.log(fin, est, sw, den, nor);
+// let [fin, est, sw, den, nor] = countries;
+// console.log(fin, est, sw, den, nor);
 
 // 3 Destructure the rectangle object by its properties or keys.
-let { width, height, area, perimeter } = rectangle;
-console.log('width', width);
-console.log('height', height);
-console.log('area', area);
-console.log('perimeter', perimeter);
+// let { width, height, area, perimeter } = rectangle;
+// console.log('width', width);
+// console.log('height', height);
+// console.log('area', area);
+// console.log('perimeter', perimeter);
 
 //! Lv-2
 /* 1 Iterate through the users array and get all the keys of the object using
@@ -269,3 +270,71 @@ destructuring */
 // console.log(usersWithLessTwoSkills);
 
 //! Lv3
+// 1 - Destructure the countries object print name, capital, population and
+// languages of all countries
+// for (const { name, capital, population, languages } of countries) {
+//   console.log(
+//     `Country: ${name}, capital: ${capital}, population: ${population}, language(s): ${languages}`
+//   );
+// }
+/* 2 A junior developer structure student name, skills and score in array of 
+arrays which may not easy to read. Destructure the following array name to 
+name, skills array to skills, scores array to scores, JavaScript score to 
+jsScore and React score to reactScore variable in one line. */
+// const student = ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]]
+// let [name, skills, scores] = student
+// let jsScore = scores[2]
+// let reactScore = scores[3]
+
+// console.log(name, skills, jsScore, reactScore)
+
+/*3 - Write a function called convertArrayToObject which can convert the array to
+a structure object.*/
+// const students = [
+//   ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]],
+//   ['John', ['HTM', 'CSS', 'JS', 'React'], [85, 80, 85, 80]],
+// ];
+
+// const convertArrayToObject = (array) => {
+//   const students = [];
+//   for (const [name, skills, scores] of array) {
+//     students.push({ name: name, skills: skills, scores: scores });
+//   }
+//   return students;
+// };
+// console.log(convertArrayToObject(students));
+
+/* 4- Copy the student object to newStudent without mutating the original object. 
+In the new object add the following ?
+Add Bootstrap with level 8 to the front end skill sets
+Add Express with level 9 to the back end skill sets
+Add SQL with level 8 to the data base skill sets
+Add SQL without level to the data science skill sets */
+const student = {
+  name: 'David',
+  age: 25,
+  skills: {
+    frontEnd: [
+      { skill: 'HTML', level: 10 },
+      { skill: 'CSS', level: 8 },
+      { skill: 'JS', level: 8 },
+      { skill: 'React', level: 9 },
+    ],
+    backEnd: [
+      { skill: 'Node', level: 7 },
+      { skill: 'GraphQL', level: 8 },
+    ],
+    dataBase: [{ skill: 'MongoDB', level: 7.5 }],
+    dataScience: ['Python', 'R', 'D3.js'],
+  },
+};
+const newStudent = {
+  ...student,
+  skills: {
+    frontEnd: [...student.skills.frontEnd, { skill: 'Bootstrap', level: 8 }],
+    backEnd: [...student.skills.backEnd, { skill: 'Express', level: 9 }],
+    dataBase: [...student.skills.dataBase, { skill: 'SQL', level: 8 }],
+    dataScience: [...student.skills.dataScience, 'SQL'],
+  },
+};
+console.log(newStudent);

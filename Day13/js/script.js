33d4072354e4437ac1ca4a8fd11b44195471fe43
Julio Zampier <juliozampier@gmail.com>
2023-06-02 16:37:00 -0300

    Day 13 substititution

--- Day13/js/script.js
+++ Day13/js/script.js
@@ -1,1 +1,20 @@
-console.log('Hello World');
+//Showing output on browser console
+console.log('30 Days of JavaScript');
+
+//Substitution
+console.log('%d %s of JavaScript', 30, 'Days');
+let day = 30;
+let txt = 'Days';
+console.log('%d %s of JavaScript', day, txt);
+
+//CSS
+//Styling
+console.log('%c30 Days of JavaScript', 'color: red; font-size: 30px');
+console.log(
+  '%c30 Days%c %cOf%c %cJavaScript%c',
+  'color:green',
+  '',
+  'color:red',
+  '',
+  'color:yellow'
+);

841c2193668cf69b5af18aab9ceedc204064b294
Julio Zampier <juliozampier@gmail.com>
2023-06-03 12:10:15 -0300

    Day 15 classes - constructor

--- Day15/js/script.js
+++ Day15/js/script.js
@@ -4,6 +4,39 @@ class ClassName {
   //  code goes here
 }
 // example
-class Person{
-  // code goes here
-}
+// class Person {
+//   // code goes here
+// }
+// const jadson = new Person();
+// console.log(jadson);
+
+//? Class constructor
+// class Person {
+//   constructor(firstName, lastName) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//   }
+// }
+// const jadson = new Person('Nosdaj', 'Arievilo');
+// const julio = new Person('Julio', 'Zampier');
+// const Kemmel = new Person('Kemmel', 'Pinto');
+
+// console.log(jadson);
+// console.log(julio);
+// console.log(Kemmel);
+
+// class Person {
+//   constructor(firstName, lastName, age, country, city) {
+//     console.log(this);
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.age = age;
+//     this.country = country;
+//     this.city = city;
+//   }
+// }
+
+// const pessoa1 = new Person('Nosdaj', 'Arievilo', 52, 'Lhorien', 'Valfenda');
+// console.log(pessoa1);
+
+//? Default values with constructor

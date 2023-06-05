8f9301b4462965ded7a2b6e6c8b4bc87de2018b3
Julio Zampier <juliozampier@gmail.com>
2023-06-04 23:17:55 -0300

    Day 17 Done

--- Day17/js/script.js
+++ Day17/js/script.js
@@ -1,11 +1,12 @@
+//string
 localStorage.setItem('firstName', 'Nosdaj');
-
+//number
 localStorage.setItem('age', 1230);
-
+//array of strings
 let skills = ['HTML', 'CSS', 'JS', 'React'];
 let skillsJSON = JSON.stringify(skills);
 localStorage.setItem('skills', skillsJSON);
-
+//array of objects
 skills = [
   { tech: 'HTML', level: 10 },
   { tech: 'CSS', level: 9 },
@@ -17,4 +18,39 @@ skills = [
 ];
 skillsJSON = JSON.stringify(skills);
 localStorage.setItem('skills', skillsJSON);
+
+// Object
+const user = {
+  firstName: 'Nosdaj',
+  age: 1254,
+  skills: ['WoW', 'WeakAuras', 'AHTrading', 'Speaks Sindarin'],
+};
+const userJSON = JSON.stringify(user);
+localStorage.setItem('user', userJSON);
+
 console.log(localStorage);
+
+//? Getting item from localStorage
+//* syntax: localStorage.getItem('key')
+
+let firstName = localStorage.getItem('firstName');
+let age = localStorage.getItem('age');
+let skillSet = localStorage.getItem('skills');
+let skillObj = JSON.parse(skillSet);
+
+console.log(firstName, age, skillObj);
+localStorage.removeItem('firstName');
+localStorage.removeItem('age');
+localStorage.clear();
+
+//! Exercises
+
+const oCara = {
+  firstname: 'Jadson',
+  lastLame: 'The Oliver',
+  age: '3033',
+  country: 'Lhorien',
+  city: 'Valfenda',
+};
+const oCaraTxt = JSON.stringify(oCara);
+localStorage.setItem('oCara', oCaraTxt);

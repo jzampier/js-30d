8d8236e7f688a0f15d0a2e53d59914e66d65cbf2
Julio Zampier <juliozampier@gmail.com>
2023-06-06 10:45:02 -0300

    Day18 Promises

---
+++ Day18/js/script.js
@@ -0,0 +1,13 @@
+//! Promises
+//* Promises are used to handle asynchronous operations in JavaScript.
+//? Syntax
+/* const myPromise = new Promise((resolve, reject)=>{
+    if(condition){
+        resolve('Deu certo! Funcionou!')
+    }else{
+        reject('Nao deu certo! Nao funcionou')
+    }
+})
+myPromise.then((data)=>{
+  console.log(data)
+}) */

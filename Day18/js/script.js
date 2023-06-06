d694b4ea75726b69fea4c581bca53c747b6c0f5c
Julio Zampier <juliozampier@gmail.com>
2023-06-06 11:58:03 -0300

    Day18 Exercises

--- Day18/js/script.js
+++ Day18/js/script.js
@@ -8,6 +8,118 @@
         reject('Nao deu certo! Nao funcionou')
     }
 })
+* qdo o myPromise for resolvido, ele vai chamar o resolve
 myPromise.then((data)=>{
   console.log(data)
-}) */
+})
+* qdo o myPromise for rejeitado, ele vai chamar o reject com catch
+myPromise.then((data)=>{}).catch((err)=>{})
+*/
+//? Callbacks
+/* 
+const doSomething = (callback) => {
+  setTimeout(() => {
+    const skills = ['JavaScript', 'HTML', 'CSS'];
+    //callback('It did not go well', skills); //? err is true
+    callback(false, skills);
+  }, 2000);
+};
+const callback = (err, result) => {
+  if (err) {
+    return console.error(err);
+  }
+  return console.log(result);
+};
+doSomething(callback);
+*/
+
+//? Promise constructor
+
+// const myPromise = new Promise((resolve, reject) => {
+//   resolve('Deu certo!');
+//   reject('Fudeu!');
+// });
+
+//? Example
+/* 
+const doPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    //const skills = ['JavaScript', 'HTML', 'CSS'];
+    const skills = [];
+    if (skills.length > 0) {
+      resolve(skills);
+    } else {
+      reject('Algo errado nao deu certo');
+    }
+  }, 2000);
+});
+doPromise
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+ */
+// const doPromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     const skills = ['JavaScript', 'HTML', 'CSS'];
+//     if (skills.includes('Node')) {
+//       resolve('fullstack developer');
+//     } else {
+//       reject('Something wrong has happened');
+//     }
+//   }, 2000);
+// });
+// doPromise
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+//! Fetch API
+//* Fetch API is used to make HTTP requests.
+
+// const url = 'https://restcountries.com/v2/all';
+// fetch(url)
+//   .then((response) => response.json())
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+//! Async/Await
+//* Async/Await is a syntax sugar for promises. (an elegant way to handle promises)
+
+// const square = async function (n) {
+//   return n * n;
+// };
+// const calcSq = async function (num) {
+//   const value = await square(num);
+//   console.log(value);
+// };
+// calcSq(5);
+
+//?exampies
+
+// const url = 'https://restcountries.com/v2/all';
+// const fetchData = async () => {
+//   try {
+//     const response = await fetch(url);
+//     const countries = await response.json();
+//     console.log(countries);
+//   } catch (err) {
+//     console.error(err);
+//   }
+// };
+// console.warn('===== async and await');
+// fetchData();
+
+//! Day 18 Exercises
+//! Lv1
+//1 - Read the countries API using fetch and print the name of country, capital,
+// languages, population and area.

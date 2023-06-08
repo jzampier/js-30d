d4e5a7b08e9cb5ef88ed79d4106495816dfe3c1d
Julio Zampier <juliozampier@gmail.com>
2023-06-08 19:39:56 -0300

    Day 21 - dom project1 ongoing

--- Day21/js/proj1.js
+++ Day21/js/proj1.js
@@ -1,1 +1,16 @@
-console.warn('horla mondio');
+//generates random colors
+function generateRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+const changingText = document.getElementById('changingText');
+
+setInterval(() => {
+  const randomColor = generateRandomColor();
+  changingText.style.color = randomColor;
+}, 1000);

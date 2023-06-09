458115459c12cd092015bffcf410d017b2363b84
Julio Zampier <juliozampier@gmail.com>
2023-06-09 00:28:18 -0300

    Day 21 - dom project1 ongoing

--- Day21/js/proj1.js
+++ Day21/js/proj1.js
@@ -1,4 +1,18 @@
-//generates random colors
+// function generateRandomColor() {
+//   const letters = '0123456789ABCDEF';
+//   let color = '#';
+//   for (let i = 0; i < 6; i++) {
+//     color += letters[Math.floor(Math.random() * 16)];
+//   }
+//   return color;
+// }
+// function changeTextColor(text) {
+//   const randomColor = generateRandomColor();
+//   text.style.color = randomColor;
+// }
+// const year = document.querySelector('#year');
+// setInterval(changeTextColor(year), 1000);
+
 function generateRandomColor() {
   const letters = '0123456789ABCDEF';
   let color = '#';
@@ -8,9 +22,56 @@ function generateRandomColor() {
   return color;
 }
 
-const changingText = document.getElementById('changingText');
+function changeTextColor(textElement) {
+  const randomColor = generateRandomColor();
+  textElement.style.color = randomColor;
+}
+
+function updateDateAndTime(containerElement) {
+  const currentTime = new Date();
+  containerElement.innerText = currentTime.toLocaleString();
+  const randomColor = generateRandomColor();
+  containerElement.style.backgroundColor = randomColor;
+}
+
+function tasks() {
+  updateDateAndTime(dateAndTime);
+  changeTextColor(year);
+}
+
+// get parent element
+const wrapper = document.querySelector('.wrapper');
+const year = document.querySelector('#year');
+const list1 = wrapper.querySelector('ul');
+// get all li spans
+const liSpans = document.querySelectorAll('li span');
+// create date and time element
+const dateAndTime = document.createElement('span');
+dateAndTime.className = 'outer-span';
+dateAndTime.id = 'date-and-time';
+
+const testListElement = document.querySelector('#le-1');
+testListElement.style.backgroundColor = '#EB695B';
+testListElement.style.fontSize = '20px';
+testListElement.style.borderLeft = '10px solid #EB695B';
+testListElement.style.borderTop = '20px solid #EB695B';
+testListElement.style.borderBottom = '20px solid #EB695B';
+testListElement.style.paddingRight = '350px';
+testListElement.style.margin = '5px';
+testListElement.style.listStyleType = 'none';
+// add date and time element to parent element
+wrapper.insertBefore(dateAndTime, list1);
+
+//? styles
+//wrapper
+wrapper.style.display = 'grid';
+wrapper.style.justifyItems = 'center';
+//wrapper
+
+year.style.fontSize = '60px';
+year.style.fontFamily = 'Sans';
+dateAndTime.style.fontSize = '20px';
 
 setInterval(() => {
-  const randomColor = generateRandomColor();
-  changingText.style.color = randomColor;
+  tasks();
 }, 1000);
